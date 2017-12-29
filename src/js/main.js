7a4178bf6a5b8fbae2904c4fbdb179d366229fd0
Natusya <natmihkr@gmail.com>
2017-12-30 00:47:55 +0100

    js for video

--- src/js/main.js
+++ src/js/main.js
@@ -1,98 +1,105 @@
 const video = document.getElementById('video')
-const playPause = document.getElementById('playpause')
-var timerWrapper = document.getElementById('timerWrapper')
-//propgress
-var timerBar = document.getElementById('timerBar')
-var timerLine = document.getElementById('timerLine')
-var currentTime = document.getElementById('currentTime')
-var duration = document.getElementById('duration')
-var volumeBar = document.getElementById('volumeBar')
-var volumeLine = document.getElementById('volumeLine')
-var volumeWrapper = document.getElementById('volumeWrapper')
 
-function playPauseMedia() {
-  if (video.paused) {
-    playPause.className = 'play'
-    return video.play()
-  }
-  playPause.className = 'pause'
-  video.pause()
-}
+function videoLoaded () {
+  const playPause = document.getElementById('playpause')
+  const currentTime = document.getElementById('currentTime')
+  const duration = document.getElementById('duration')
+  const timerWrapper = document.getElementById('timerWrapper')
+  const volumeWrapper = document.getElementById('volumeWrapper')
+  const timerBar = document.getElementById('timerBar')
+  const timerLine = document.getElementById('timerLine')
+  const volumeBar = document.getElementById('volumeBar')
+  const volumeLine = document.getElementById('volumeLine')
 
-function setTime() {
-  var minutes = Math.floor(video.currentTime / 60)
-  var seconds = Math.floor(video.currentTime - minutes * 60)
-  var minuteValue
-  var secondValue
-
-  if (minutes < 10) {
-    minuteValue = '0' + minutes
-  } else {
-    minuteValue = minutes
+  function playPauseMedia() {
+    if (video.paused) {
+      playPause.className = 'play'
+      return video.play()
+    }
+    playPause.className = 'pause'
+    video.pause()
   }
 
-  if (seconds < 10) {
-    secondValue = '0' + seconds
-  } else {
-    secondValue = seconds
+  function formatDuration (duration) {
+    const minutes = Math.floor(duration / 60)
+    const seconds = Math.floor(duration - minutes * 60)
+    let minuteValue
+    let secondValue
+
+    if (minutes < 10) {
+      minuteValue = '0' + minutes
+    } else {
+      minuteValue = minutes
+    }
+
+    if (seconds < 10) {
+      secondValue = '0' + seconds
+    } else {
+      secondValue = seconds
+    }
+
+    return `${minuteValue} : ${secondValue}`
   }
 
-  var mediaTime = minuteValue + ':' + secondValue
+  function updateProgressBar (all, current, wrapper, bar) {
+    const barLength = wrapper.clientWidth * (current/all)
+    bar.style.width = barLength + 'px'
+  }
 
-  currentTime.textContent = mediaTime
+  function setTime() {
+    currentTime.textContent = formatDuration(video.currentTime)
+    updateProgressBar(video.duration, video.currentTime, timerWrapper, timerBar)
+  }
 
-  var barLength = timerWrapper.clientWidth * (video.currentTime/video.duration)
-  timerBar.style.width = barLength + 'px'
-}
+  function updateVolumeBar (volume) {
+    updateProgressBar(1, volume, volumeWrapper, volumeBar)
+  }
 
-function videoDurationTime () {
-  var all = Math.floor(video.duration)
-  var minutes = Math.floor(all / 60);
-  var seconds = Math.floor(all - minutes * 60);
-  var mediaTime = minutes + ':' + seconds
-  duration.textContent = mediaTime
+  function onProgressClick (maxVal, cb) {
+    return function (event) {
+      const rect = event.target.getBoundingClientRect()
+      const position = (event.offsetX * maxVal) / rect.width
+
+      cb(position)
+    }
+  }
+
+  function updateVideoTime (time) {
+    video.currentTime = time
+  }
+
+  function updateVideoVolume (volume) {
+    updateVolumeBar(volume)
+    video.volume = volume
+  }
+
+  function fullScreenButton() {
+    if (video.requestFullscreen) {
+      video.requestFullscreen()
+    } else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen()
+    } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen()
+    }
+  }
+
   updateVolumeBar(video.volume)
+
+  duration.textContent = formatDuration(video.duration)
+
   timerLine.addEventListener('click', onProgressClick(video.duration, updateVideoTime))
   volumeLine.addEventListener('click', onProgressClick(1, updateVideoVolume))
+  playPause.addEventListener('click', playPauseMedia)
+  video.addEventListener('timeupdate', setTime)
+  document.getElementById('fullScreen').onclick = fullScreenButton
 }
 
-function updateVolumeBar (volume) {
-  var barLength = volumeWrapper.clientWidth * (volume/1)
-  volumeBar.style.width = barLength + 'px'
-}
-
-function onProgressClick (maxVal, cb) {
-  return function (event) {
-    const rect = event.target.getBoundingClientRect()
-    const position = (event.offsetX * maxVal) / rect.width
-
-    cb(position)
+(function (videoEl) {
+  if (videoEl.readyState === 0) {
+    videoEl.addEventListener( 'loadedmetadata', function () {
+      videoLoaded()
+    })
+  } else if (videoEl.readyState >= 1) {
+    videoLoaded()
   }
-}
-
-function updateVideoTime (time) {
-  video.currentTime = time
-}
-
-function updateVideoVolume (volume) {
-  updateVolumeBar(volume)
-  video.volume = volume
-}
-
-function fullScreenButton() {
-  if (video.requestFullscreen) {
-    video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-    video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-    video.webkitRequestFullscreen();
-  }}
-
-document.getElementById("fullScreen").onclick = function() {
-  fullScreenButton()
-};
-
-playPause.addEventListener('click', playPauseMedia)
-video.addEventListener('timeupdate', setTime)
-video.addEventListener('loadedmetadata', videoDurationTime)
-
+})(video)
